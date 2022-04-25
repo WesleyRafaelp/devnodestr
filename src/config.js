f66b46f3c9712c65a837d5f78a23c18ed63afed2
Wesley Rafael <103283531+WesleyRafaelp@users.noreply.github.com>
2022-04-25 10:56:35 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString :'mongodb://root:admin123@localhost:27017',
-    sendgridKey: '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',
-    containerConnectionString:'DefaultEndpointsProtocol=https;AccountName=devnodestore;AccountKey=gCaEH8IUuyQgovlJcu/rvyT/AXMzfnV09Xtb69GPChgR3HJ3vUHaPqbuZX9vUQbByb1RqyjfDzP+aLFyXLl3Aw==;EndpointSuffix=core.windows.net'
+    connectionString :'',
+    sendgridKey: '',
+    containerConnectionString:''
 };
